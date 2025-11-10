2d22cf7d595d959c9276fc194c9c4f453b0f3590
hthere1209 <htherecrane@gmail.com>
2025-11-10 06:14:20 +0300

    manual set proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: process.env.REACT_APP_BACKEND_URL,
+      target: "https://ronnel-talented-dev-backend.onrender.com",
       changeOrigin: true,
     })
   );
